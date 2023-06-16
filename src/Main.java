c0215544cd61c6c6f5f7428ac9accf4989943717
LucaDalRi <euro2xd@gmail.com>
2023-06-16 14:59:05 +0200

    Milestone 2 completata

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/db-nations";
+        String user = "😊";
+        String pswd = "😊";
 
 
+        try (Connection con = DriverManager.getConnection(url, user, pswd)) {
 
+            String query = """
+                    SELECT countries.name nome_nazione , countries.country_id id_nazione , regions.name nome_regione , continents.name nome_continente
+                    FROM countries
+                    INNER JOIN regions ON countries.region_id = regions.region_id
+                    INNER JOIN continents ON continents.continent_id = regions.continent_id
+                    ORDER BY countries.name ASC;
+                    """;
 
+            try (PreparedStatement ps = con.prepareStatement(query)) {
+
+                try (ResultSet rs = ps.executeQuery()) {
+
+                    while (rs.next()) {
+
+                        String nomeNazione = rs.getString(1);
+                        int idNazione = rs.getInt(2);
+                        String nomeRegione = rs.getString(3);
+                        String nomeContinente = rs.getString(4);
+
+                        System.out.println("nome nazione - " + nomeNazione + " / ID Nazione - " + idNazione + " / Nome Regione - " + nomeRegione + " / Nome Continente - " + nomeContinente);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
